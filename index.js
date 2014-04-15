4107c04399f75c0c37cc6af65f80a8d8bd4230f6
Kenan Yildirim <kenan@kenany.me>
2014-04-15 16:32:48 -0400

    Use behest
    Closes #1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var behest = require('behest');
+
 function plugin() {
   return function(irc) {
     irc.on('message', function(evt) {
@@ -5,29 +7,30 @@ function plugin() {
       var to = evt.to;
       var message = evt.message;
 
-      if (message.charAt(0) === '!') {
-        var params = message.split(' ');
-        var command = params[0].replace('!', '');
-        if (command === 'base64') {
-          var destination = to.charAt(0) === '#' ? to : from;
-          var str = message.substring(15);
-          var direction = params[1];
+      if (!behest.isValid(message)) {
+        return;
+      }
 
-          var b;
-          switch (direction) {
-            case 'encode':
-              b = new Buffer(str).toString('base64');
-              break;
-            case 'decode':
-              b = new Buffer(str, 'base64').toString('utf8');
-              break;
-            default:
-              irc.send(destination, 'Huh? Try `!base64 encode|decode <query>`');
-          }
+      var command = behest(message);
 
-          if (b) {
-            irc.send(destination, from + ': ' + b);
-          }
+      if (command.command === 'base64') {
+        var destination = to.charAt(0) === '#' ? to : from;
+        var str = command.params.slice(1).join(' ');
+
+        var b;
+        switch (command.params[0]) {
+          case 'encode':
+            b = new Buffer(str).toString('base64');
+            break;
+          case 'decode':
+            b = new Buffer(str, 'base64').toString('utf8');
+            break;
+          default:
+            irc.send(destination, 'Huh? Try `!base64 encode|decode <query>`');
+        }
+
+        if (b) {
+          irc.send(destination, from + ': ' + b);
         }
       }
     });
